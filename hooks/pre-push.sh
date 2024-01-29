def548f8ed9ca8b36227cd1310732ea9b97fc111
hxsm <romain.alexandre5@orange.fr>
2024-01-29 16:42:47 +0100

    Modification pre-commit et pre-push

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./run_tests.sh
+# Exécuter les tests PHPUnit
+docker compose exec php bin/phpunit
+
+# Exécuter PHPStan pour l'analyse statique
+docker compose exec php vendor/bin/phpstan analyze
+
+# Exécuter PHPCS pour le contrôle de la qualité du code
+docker compose exec php vendor/bin/phpcs
+
+# Exécuter les tests unitaires avec PHPUnit
+docker compose exec php bin/phpunit --testsuite unit
+
